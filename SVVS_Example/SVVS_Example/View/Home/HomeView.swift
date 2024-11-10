ab17c26fdab28df8ebd9e6bd422a6cf3bc5d9f37
y-hirakaw <130817538+y-hirakaw@users.noreply.github.com>
2024-11-10 21:37:06 +0900

    ハンズオン用に実装を削る

--- SVVS_Example/SVVS_Example/View/Home/HomeView.swift
+++ SVVS_Example/SVVS_Example/View/Home/HomeView.swift
@@ -1,15 +1,10 @@
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject private var state: HomeViewState
-
-    init(state: HomeViewState = HomeViewState()) {
-        self._state = .init(wrappedValue: state)
-    }
 
     var body: some View {
         VStack {
-            Text(self.state.userGreeting)
+            Text("TODO: 「こんにちは {ユーザ名}さん」と表示してください。")
         }
     }
 }
--- SVVS_Example/SVVS_Example/View/Home/HomeViewSate.swift
+++ SVVS_Example/SVVS_Example/View/Home/HomeViewSate.swift
@@ -3,28 +3,5 @@ import Foundation
 
 @MainActor
 class HomeViewState: ObservableObject {
-    private let store: UserStore
-    @Published var user: User?
 
-    private var cancellables = Set<AnyCancellable>()
-
-    init(store: UserStore = .shared) {
-        self.store = store
-        self.setupStoreBindings()
-    }
-
-    /// Storeプロパティ購読を設定する
-    func setupStoreBindings() {
-        self.store.$user
-            .sink { [weak self] user in
-                self?.user = user
-            }
-            .store(in: &cancellables)
-    }
-
-    /// ユーザ名付きの挨拶文字列
-    var userGreeting: String {
-        guard let user else { return "名前が取得できません" }
-        return "こんにちは \(user.name)さん"
-    }
 }
